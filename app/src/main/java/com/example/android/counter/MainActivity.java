aedf52407e9207a1dc3c3d9d3e55ac6d990fec3a
aprybysh <alekspribysh@gmail.com>
2016-01-29 13:45:05 -0800

    Initial commit

--- app/src/main/java/com/example/android/counter/MainActivity.java
+++ app/src/main/java/com/example/android/counter/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -41,4 +42,97 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+    private  int scoreA;
+    private  int scoreB;
+    private  String vinner;
+
+    public void tpointsA (View view) {
+
+        scoreA = scoreA +3;
+        displayScoreA(scoreA);
+    }
+
+    public void twpointsA (View view) {
+
+        scoreA = scoreA +2;
+        displayScoreA(scoreA);
+    }
+
+    public void fpointsA (View view) {
+
+        scoreA = scoreA +1;
+        displayScoreA(scoreA);
+    }
+
+
+
+    public void tpointsB (View view) {
+
+        scoreB = scoreB +3;
+        displayScoreB(scoreB);
+    }
+
+    public void twpointsB (View view) {
+
+        scoreB = scoreB +2;
+        displayScoreB(scoreB);
+    }
+
+    public void fpointsB (View view) {
+
+        scoreB = scoreB +1;
+        displayScoreB(scoreB);
+    }
+
+    public void displayViner(View v){
+        if (scoreA > scoreB) {
+            vinner = " Vinner Team A";
+        }
+
+        if (scoreA < scoreB) {
+            vinner = " Vinner Team B";
+        }
+
+        if (scoreA == scoreB ){
+           vinner = "DRAW";
+        }
+
+        displayVinner(vinner);
+    }
+
+    public void reset(View v){
+
+        scoreA = 0;
+        scoreB =0;
+        vinner = "";
+        displayVinner(vinner);
+        displayScoreA(scoreA);
+        displayScoreB(scoreB);
+    }
+
+
+    public void displayScoreA(int a){
+        TextView scoreA = (TextView) findViewById(
+                R.id.scoreA
+        );
+        scoreA.setText(String.valueOf(a));
+    }
+
+    public void displayScoreB(int a){
+        TextView scoreB = (TextView) findViewById(
+                R.id.scoreB
+        );
+        scoreB.setText(String.valueOf(a));
+    }
+
+    public void displayVinner(String str){
+
+        TextView vinner = (TextView) findViewById(
+                R.id.showVinner);
+        vinner.setText(str);
+    }
+
+
 }
